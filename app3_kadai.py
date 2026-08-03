4ae094e7f2ede1a21b94e34c42bf51ddfce5248d
itc-s26006 <s26006@std.it-college.ac.jp>
2026-08-03 17:02:42 +0900

    app3_kadai.py終了

---
+++ app3_kadai.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+def displabel():
+    lol.configurez(text="初めてのアプリ")
+
+root = tk.Tk()
+root = geometry("300x400"):
+
+
+lbl2=tk.Label(text="初めてのアプリ")
+lbl=tk.Label(text="LABEL")
+btn = tk.botton(text="push",command =,displayLabel):
+
+lbl2 = tk.label(text="はじめてのアプリ")
+lbl= tk.label(text="LABEL")
+btn=tk.button(text="push",command=dispLabel)
+
+lbl2.pack()
+lbl.pack()
+btn.pack()
+tk.mainloop()
